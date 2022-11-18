55ba65005713da07d01c1c0daf34dabf58adc21c
Nordleen De Frias <nordleendefrias@gmail.com>
2022-11-17 21:13:02 -0500

    Committing initial script.js + Index.html + Readme.md files

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# server-side-API-weather-dashboard
+# Server Side API - Weather Dashboard
+ 
+### **Description**
+> #### This an application returns weather information from a city when input is provided under search for a city. One can also retrieve the information using the buttons to search for a city. When a city is display the five-day forecast is also displayed under five day forecast column.
+
+### **Aplication Link**
+* [GitHub Aplication Link]()
+
+## **Table of Contents**
+- [Project Name](#project_name)
+- [How to install it](#installation)
+- [Running Tests](#tests)
+- [How to use](#usage)
+- [Report issues](#issues)
+- [Questions](#username)
+- [How to make contributions](#contributions)
+- [Credits](#credits)
+- [License](#license)
+### **Project Name**
+* Weather Dashboard Application
+### **Installation**
+* N/A
+### **Tests**
+* N/A
+### **Usage**
+* You can use the application to request weather information.
+### **Questions**
+* Contact me with questions via [GitHub](https://github.com/NDF-WEB-DEV) or send an email to: nordleendefrias@gmail.com
+### **Report Issues**
+* Send me an email
+### **Contributions**
+* This is a school project.
+### **Credits**
+* [Open Weather API](https://openweathermap.org/)
+### **License**
+* N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,40 @@
-var cityResultFiveDays = document.getElementById("#city-result-five-days");
-var userForm = document.getElementById("#user-form");
+var cityResultFiveDays = document.getElementById("city-result-five-days");
+var userForm = document.getElementById("user-form");
 var cityButtonResults = document.getAttribute("city-info");
-var userFormElement = document.getElementById("#user-form");
-var cityTitle = document.getElementById("city-title");
+var userFormElement = document.getElementById("user-form");
 var theTemperature = document.getElementById("temperature");
 var theWind = document.getElementById("wind");
 var theHumidity = document.getElementById("humidity");
 var theUvindex = document.getElementById("uvindex");
 
+// ============================== HANDLER (INPUT) - function handles submit search for city and city buttons
+function searchHandlingEvent(event) {
+    event.preventDefault();
+
+    var searchCity = document.getElementById('searchCityName').value;
+    if(!searchCity) {
+        console.error('You need to input a City Name!');
+        return;
+    }
+    apiSearchByInput(searchCity);
+};
+
+userFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    searchHandlingEvent();
+});
+
 // ============================== INPUT - function search on OpenWeather One Call API
-function apiSearchByInput() {
+function apiSearchByInput(city) {
+    console.log("hi, I am searching by input");
+
+    var cityTitle = document.getElementById("city-title");
+    cityTitle.innerHTML = city.value;
+    console.log(city);
+    console.log(cityTitle);
+    const cityLimits = 5;
+    const cityButtonKey = "8d7e2d7b64a9b790b3ae603f52ea3086";
+
     var requestOptions;
     var myHeaders = new Headers();
     myHeaders.append("apiKeys", "8d7e2d7b64a9b790b3ae603f52ea3086");
@@ -20,73 +45,40 @@ function apiSearchByInput() {
     };
     console.log(myHeaders);
     console.log(requestOptions);
+    console.log(city);
 
     // City Search Input
-    var citySearchValue = document.querySelector("searchCityName").value.trim();
+    var citySearchValue = document.getElementById("searchCityName").value.trim();
     console.log(citySearchValue);
 
     //example raw call: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=citySearchValue&limit=5&appid=8d7e2d7b64a9b790b3ae603f52ea3086")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&limit=" + cityLimits + "&appid=" + cityButtonKey, requestOptions)
         .then(response => response.json())
         .then(result => {
-            result.result = cityResultFiveDays.innerHTML;
-            cityResultFiveDays.textContent = result.result;
+            result = cityResultFiveDays.innerHTML;
+            cityResultFiveDays.textContent;
+            console.log(cityResultFiveDays);
         })
         .catch(error => console.log('error', error));
-        console.log(result);
 
-        //displaying results on page
-        cityTitle.textContent = citySearchValue;
-        theTemperature.textContent = temperature;
+        console.log(result);
+        console.log(myHeaders);
+        console.log(requestOptions);
+        console.log(city);
+
+        //displaying results on page for 1 city
+        cityTitle.textContent = city;
+        theTemperature.textContent = theTemperature;
         theWind.textContent = wind;
         theHumidity.textContent = humidity;
         theUvindex.textContent = uvindex;
 
         console.log(citySearchValue);
-        console.log(citySearchValue);
-        console.log(citySearchValue);
-        console.log(citySearchValue);
-        console.log(citySearchValue);
+        console.log(temperature);
+        console.log(wind);
+        console.log(humidity);
+        console.log(uvindex);
 }
-
-// ============================== BUTTON - function search on OpenWeather One Call API
-function apiSearchByBtn(citySearchButton) {
-    var requestOptions;
-    var myHeaders = new Headers();
-    myHeaders.append("apiKeys", "c542be447bb6ea8c00c93c8f4bd63050");  // different key from input function
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow',
-        Headers: 'myHeaders'
-    };
-    console.log(myHeaders);
-    console.log(requestOptions);
-
-    // City Search by Button
-    var citySearchButton = document.getAttribute("city-info").value;
-    console.log(citySearchButton);
-
-    //example raw call: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=citySearchButton&limit=5&appid=c542be447bb6ea8c00c93c8f4bd63050")
-        .then(response => response.json())
-        .then(result => {
-            result.result = cityResultFiveDays.innerHTML;
-            cityResultFiveDays.textContent = result.result;
-        })
-        .catch(error => console.log('error', error));
-        console.log(result);
-}
-
-// ============================== HANDLER (INPUT) - function handles submit search for city and city buttons
-function searchHandlingEvent(event) {
-    event.preventDefault();
-
-    var searchCity = document.querySelector('#searchCityName').value;
-    if(!searchCity) {
-        console.error('You need to input a City Name!');
-        return;
-    }
-};
 
 // ============================== HANDLER (BUTTON) - function handles submit search for city and city buttons
 function buttonHandlingEvent(event) {
@@ -99,5 +91,39 @@ function buttonHandlingEvent(event) {
     }
 };
 
-userFormElement.addEventListener('submit', searchHandlingEvent);
-buttonCitySelectionElement.addEventListener('click', searchHandlingEvent);
+buttonCitySelectionElement.addEventListener('click', function(event){
+    event.preventDefault();
+    buttonHandlingEvent();
+});
+
+// ============================== BUTTON - function search on OpenWeather One Call API
+function apiSearchByBtn(citySearchButton) {
+    console.log("hi, I am searching by button");
+
+    const buttonSearchApiKey = "c542be447bb6ea8c00c93c8f4bd63050";
+
+    var requestOptions;
+    var myHeaders = new Headers();
+    myHeaders.append("apiKeys", "c542be447bb6ea8c00c93c8f4bd63050");  // different key from input function
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        Headers: 'myHeaders'
+    };
+    console.log(myHeaders);
+    console.log(requestOptions);
+
+    // City Search by Button
+    var citySearchButton = document.getAttribute("city-info").value;  //
+    console.log(citySearchButton);
+
+    //example raw call: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + citySearchButton + "&appid=" + buttonSearchApiKey, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            result = cityResultFiveDays.innerHTML;
+            cityResultFiveDays.textContent;
+        })
+        .catch(error => console.log('error', error));
+        console.log(result);
+};
